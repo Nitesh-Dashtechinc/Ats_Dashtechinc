6ed67abc89af21259a281bf9e586324a20928aa3
jignesh parmar <jignesh.dashtechinc@gmail.com>
2019-02-26 11:58:06 +0530

    Jignesh: Fix Grid View(Index) page and added alert message in case of Exception

---
+++ Ats/Models/ViewModel/WorkFromWorkTo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ats.Models.ViewModel
+{
+    public class WorkFromWorkToViewModel
+    {
+        public int candidateId { get; set; }
+        public string WorkFrom { get; set; }
+        public string WorkTo { get; set; }
+    }
+}
